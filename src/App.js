b3fa2e0599883f1fb6d9e10ef1644285a1963534
anas siddiqui <anas91612@gmail.com>
2021-11-17 17:00:15 +0530

    comments are removed

--- src/App.js
+++ src/App.js
@@ -8,103 +8,109 @@ import 'react-toastify/dist/ReactToastify.css';
 import Covid19Page from './covid19Page';
 
 function App() {
-  const [temperature, setTemperature] = useState();
-  const [description, setDescription] = useState("");
-  const [cityName, setCityName] = useState("");
-  const [inputData, setInputData] = useState("");
-  const [emoji, setEmoji] = useState("");
+    const [temperature, setTemperature] = useState();
+    const [description, setDescription] = useState("");
+    const [cityName, setCityName] = useState("");
+    const [inputData, setInputData] = useState("");
+    const [emoji, setEmoji] = useState("");
 
 
-// useEffect(()=>{
-//   getData();
-// },[inputData]);
-const notify = () => toast.error("Opps 😥, City Not Found!");
+    // useEffect(()=>{
+    //   getData();
+    // },[inputData]);
+    const notify = () => toast.error("Opps 😥, City Not Found!");
 
 
-  function getData(){
-    axios.get(`https://api.openweathermap.org/data/2.5/find?q=${inputData}&units=metric&appid=6faebeb9d32de74e053ab7b148a867dd`).
-    then((res)=>{
-      
-      var temperature =  res.data.list[0].main.temp;
-      var description = res.data.list[0].weather[0].main;
-      var cityName = res.data.list[0].name;
-      var icon = res.data.list[0].weather[0].icon;
-      
-      if(icon=="01d" || icon=="02d"  || icon=="03d" ||icon=="04d" || icon=="01n" || icon=="02n"  || icon=="03n" ||icon=="04n"  ){
-        setEmoji("🌥"); /// clouds
-      }
-      else if(icon=="09d"  ||  icon=="09n"){
-        setEmoji("🌧")  /// rain
-      }
-      else if(icon=="11d" || icon=="11n"){
-        setEmoji("⛈")  /// thunderstorm
-      }
-      else if(icon=="13d" || icon=="13n"){
-        setEmoji("❄️")  /// snow
-      }
-      else if(icon=="50d" || icon=="50n"){
-        setEmoji("🌫️")  /// mist
-      }
+    function getData() {
+        axios.get(`https://api.openweathermap.org/data/2.5/find?q=${inputData}&units=metric&appid=6faebeb9d32de74e053ab7b148a867dd`).
+        then((res) => {
+
+            var temperature = res.data.list[0].main.temp;
+            var description = res.data.list[0].weather[0].main;
+            var cityName = res.data.list[0].name;
+            var icon = res.data.list[0].weather[0].icon;
+
+            if (icon == "01d" || icon == "02d" || icon == "03d" || icon == "04d" || icon == "01n" || icon == "02n" || icon == "03n" || icon == "04n") {
+                setEmoji("🌥"); /// clouds
+            } else if (icon == "09d" || icon == "09n") {
+                setEmoji("🌧") /// rain
+            } else if (icon == "11d" || icon == "11n") {
+                setEmoji("⛈") /// thunderstorm
+            } else if (icon == "13d" || icon == "13n") {
+                setEmoji("❄️") /// snow
+            } else if (icon == "50d" || icon == "50n") {
+                setEmoji("🌫️") /// mist
+            }
 
 
-      setTemperature(temperature + " ℃"); 
-      setCityName(cityName);
-      setDescription(description);
-    
-    }).catch((error)=>{
-      if(inputData==""){
-        toast.error("Enter city name");
-        return;
-      }
-      notify();
-    })
-  }
+            setTemperature(temperature + " ℃");
+            setCityName(cityName);
+            setDescription(description);
 
-// const randonCityNames = ['Lucknow', 'Delhi', 'Agra', 'Mumbai','Phagwara', 'Chennai', 'Pune', 'Jalandhar', 'Jaipur', 'Kanpur', 'Indore', 'Ludhiana','Amritsar'];
-// var c="";
-// const [changeName, setChageName]=useState("CITY NAME - LUCKNOW");
-// setInterval(() => {
-//    var min = 1;
-//    var max = randonCityNames.length-1;
-//    var rand =  (Math.random() * (max-min));
-//    var finalNo = Math.round(rand);
-//    c = randonCityNames[finalNo];
-//    setChageName("City name - " + c);
-// }, 3000);
+        }).catch((error) => {
+            if (inputData == "") {
+                toast.error("Enter city name");
+                return;
+            }
+            notify();
+        })
+    }
 
-  return (
-  <>
-    <div className="container">
-      <div className="row">
+    return ( <
+        >
+        <
+        div className = "container" >
+        <
+        div className = "row" >
 
-        <div className="col-lg-8">
-            <Covid19Page/>
-        </div>
-        
-        <div className="col-lg-4">
-          
-          <h1 className="text-center weather-heading">Weather Data</h1>
+        <
+        div className = "col-lg-8" >
+        <
+        Covid19Page / >
+        <
+        /div>
 
-          <div className="card shadow weather-bg">
-            <div className="col">
-              <h1 className="heading">Weatherify</h1>
-                <input type="text" className="form-control" placeholder="City name" value={inputData} onChange={(e)=>{setInputData(e.target.value)}} required/>
-                <div className="d-grid gap-2">
-                  <button onClick={getData} className="btn btn-dark">Get Weather Info</button>
-                </div>
-                <h1 className="temperature">{temperature}</h1>
-                <h1 className="cityname">{cityName}</h1>
-                <h1 className="condition"> {description} <span className="emoji">{emoji}</span></h1>
-            </div>
-          </div> 
-        </div>
-      </div>
-      <ToastContainer />
-      <p className="credit">Made with ❤️ by Anas Siddiqui</p>
-    </div>
-    </>
-  );
-  
+        <
+        div className = "col-lg-4" >
+
+        <
+        h1 className = "text-center weather-heading" > Weather Data < /h1>
+
+        <
+        div className = "card shadow weather-bg" >
+        <
+        div className = "col" >
+        <
+        h1 className = "heading" > Weatherify < /h1> <
+        input type = "text"
+        className = "form-control"
+        placeholder = "City name"
+        value = { inputData }
+        onChange = {
+            (e) => { setInputData(e.target.value) } }
+        required / >
+        <
+        div className = "d-grid gap-2" >
+        <
+        button onClick = { getData }
+        className = "btn btn-dark" > Get Weather Info < /button> <
+        /div> <
+        h1 className = "temperature" > { temperature } < /h1> <
+        h1 className = "cityname" > { cityName } < /h1> <
+        h1 className = "condition" > { description } < span className = "emoji" > { emoji } < /span></h
+        1 >
+        <
+        /div> <
+        /div>  <
+        /div> <
+        /div> <
+        ToastContainer / >
+        <
+        p className = "credit" > Made with❤️ by Anas Siddiqui < /p> <
+        /div> <
+        />
+    );
+
 }
 
 export default App;
